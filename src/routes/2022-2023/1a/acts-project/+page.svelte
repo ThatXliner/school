81b62702778e699be19449c3bb040ee92abb994f
Bryan Hu <bryan.hu.2020@gmail.com>
2023-01-13 07:19:49 -0800

    :recycle: :sparkles: Added Acts Project and refactored

---
+++ src/routes/2022-2023/1a/acts-project/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		window.location.href = 'https://thatxliner.github.io/acts-project';
+	});
+</script>
+
+<p>
+	You should be redirected to <a href="https://thatxliner.github.io/acts-project"
+		>https://thatxliner.github.io/acts-project</a
+	>. Otherwise, click that link.
+</p>
