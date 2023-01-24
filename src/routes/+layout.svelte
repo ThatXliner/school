156df0a8f7037bd4580534d63c60cb66150d0b0d
Bryan Hu <bryan.hu.2020@gmail.com>
2023-01-23 22:10:33 -0800

    Added footer with license

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,16 @@
 </script>
 
 <slot />
+<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+	<div>
+		<p>
+			Copyright © 2023-{new Date().getFullYear()} Bryan Hu (<a
+				href="https://github.com/ThatXliner"
+				class="link link-hover">@ThatXliner</a
+			>) under the
+			<a href="https://github.com/ThatXliner/school/blob/main/LICENSE.txt" class="link link-primary"
+				>GNU Affero General Public License v3+ (GNU AGPLv3+)</a
+			>
+		</p>
+	</div>
+</footer>
